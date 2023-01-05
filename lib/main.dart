98c21c305e91757849d4f48e674396951fc20e7e
Hubert Przybył <hubert5200@interia.pl>
2023-01-05 23:06:20 +0100

    Pop add screen on save

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Cant wait',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
